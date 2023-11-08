ea51254d4b2eb2320b29fa9e4ecd30cbcd97e4f4
Nguyễn Sang <sangnn8@gmail.com>
2023-11-08 22:49:19 +0700

    first commit

---
+++ src/main/java/com/se/fit/TravelProject/TravelProjectApplication.java
@@ -0,0 +1,13 @@
+package com.se.fit.TravelProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelProjectApplication.class, args);
+	}
+
+}
