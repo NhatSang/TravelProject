46b6fa802884ba1305654ef0e73ab3b4efaac8f3
Nguyễn Sang <sangnn8@gmail.com>
2023-11-12 12:57:33 +0700

    update ResutlSearchTour v1

--- src/main/java/com/se/fit/TravelProject/repository/TourRepository.java
+++ src/main/java/com/se/fit/TravelProject/repository/TourRepository.java
@@ -1,9 +1,17 @@
 package com.se.fit.TravelProject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.se.fit.TravelProject.entities.Tour;
 
 public interface TourRepository extends JpaRepository<Tour, Integer>{
 
+	@Query("SELECT t From Tour t WHERE t.departure.departureId =:departureId")
+	public List<Tour> findByDepature(@Param("departureId") int departureId);
+	@Query("SELECT t From Tour t WHERE t.destination.destinationId =:destinationId")
+	public List<Tour> findByDestination(@Param("destinationId") String destinationId);
 }
